0793ae92feef3dfe650ccb73557a10a42d1b6593
MARICOURT <pl.maricourt@hotmail.com>
2021-01-18 11:18:50 +0100

    Recipe Service: add a cross component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('Beef on a bed of potatoes', 'This is simply the best recipe', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg'),
         new Recipe('Gnocchi', 'How to make your own gnocchi', 'https://cdn.pixabay.com/photo/2015/09/16/20/10/dough-943245_960_720.jpg'),
