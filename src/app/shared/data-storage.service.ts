8fdc271d2bdee5b2cf20deee0873fb99164cf411
MARICOURT <pl.maricourt@hotmail.com>
2021-01-28 14:02:06 +0100

    Storing & Fetch recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,26 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.http.put(
+            'https://cook-95b74-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', recipes)
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
+
+    fetchRecipes(){
+        this.http.get('https://cook-95b74-default-rtdb.europe-west1.firebasedatabase.app/recipes.json').subscribe(recipes =>{
+            console.log(recipes);
+            
+        })
+    }
 }
