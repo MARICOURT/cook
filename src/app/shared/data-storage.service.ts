584a9eb8c3c551e013132ec7f85087fdc29f2eb2
MARICOURT <pl.maricourt@hotmail.com>
2021-01-28 14:33:12 +0100

    Resolving Data before Loading

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
@@ -20,14 +20,16 @@ export class DataStorageService {
     }
 
     fetchRecipes(){
-        this.http.get<Recipe[]>('https://cook-95b74-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
-        .pipe(map(recipes => {
-            return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-            });
-        }))
-        .subscribe(recipes =>{
-            this.recipeService.setRecipes(recipes);
-        })
+        return this.http.get<Recipe[]>('https://cook-95b74-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+        .pipe(
+            map(recipes => {
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+                });
+            }),
+            tap(recipes => {
+                this.recipeService.setRecipes(recipes);
+            })
+        )
     }
 }
