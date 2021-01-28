c6f30813dbe4cfdf50dc5f988a6a2dff4a8c9dbb
MARICOURT <pl.maricourt@hotmail.com>
2021-01-28 14:14:45 +0100

    Transform fetch recipe response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,32 +8,33 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Beef on a bed of potatoes', 
-            'This is simply the best recipe', 
-            'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg',
-            [
-                new Ingredient('Beef', 1),
-                new Ingredient('Potatoes', 1),
-            ]),
-        new Recipe(
-            'Gnocchi', 
-            'How to make your own gnocchi', 
-            'https://cdn.pixabay.com/photo/2015/09/16/20/10/dough-943245_960_720.jpg',
-            [
-                new Ingredient('Dough', 1),
-            ]),
-        new Recipe(
-            'Lasagna', 
-            'Delicious lasagna recipe', 
-            'https://cdn.pixabay.com/photo/2017/02/15/15/17/meal-2069021_960_720.jpg',
-            [
-                new Ingredient('Lasagna dough', 1),
-                new Ingredient('Meat', 1),
-            ])
-    ];
-
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Beef on a bed of potatoes', 
+    //         'This is simply the best recipe', 
+    //         'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg',
+    //         [
+    //             new Ingredient('Beef', 1),
+    //             new Ingredient('Potatoes', 1),
+    //         ]),
+    //     new Recipe(
+    //         'Gnocchi', 
+    //         'How to make your own gnocchi', 
+    //         'https://cdn.pixabay.com/photo/2015/09/16/20/10/dough-943245_960_720.jpg',
+    //         [
+    //             new Ingredient('Dough', 1),
+    //         ]),
+    //     new Recipe(
+    //         'Lasagna', 
+    //         'Delicious lasagna recipe', 
+    //         'https://cdn.pixabay.com/photo/2017/02/15/15/17/meal-2069021_960_720.jpg',
+    //         [
+    //             new Ingredient('Lasagna dough', 1),
+    //             new Ingredient('Meat', 1),
+    //         ])
+    // ];
+    private recipes: Recipe[] = [];
+    
     constructor(private shoppingListService: ShoppingListService){
 
     }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from 'rxjs/operators';
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
@@ -19,7 +20,13 @@ export class DataStorageService {
     }
 
     fetchRecipes(){
-        this.http.get<Recipe[]>('https://cook-95b74-default-rtdb.europe-west1.firebasedatabase.app/recipes.json').subscribe(recipes =>{
+        this.http.get<Recipe[]>('https://cook-95b74-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+        .pipe(map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+            });
+        }))
+        .subscribe(recipes =>{
             this.recipeService.setRecipes(recipes);
         })
     }
